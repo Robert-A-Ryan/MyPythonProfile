283fdb841c054719d33d162403f93ddba8efdae7
Robert Ryan <coachrobertryan@gmail.com>
2019-11-01 12:25:42 -0700

    updated static root

--- rrsite/rrsite/settings.py
+++ rrsite/rrsite/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
